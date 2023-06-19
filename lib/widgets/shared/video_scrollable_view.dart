1d4444bde97d401e480f9e64088c397a4aa63bba
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-06-19 13:30:28 -0700

    Se formatearon los numeros de los botones

--- lib/widgets/shared/video_scrollable_view.dart
+++ lib/widgets/shared/video_scrollable_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/widgets/shared/video_buttons.dart';
 import '../../domain/entities/video_post.dart';
 
 class VideoScrollablleView extends StatelessWidget {
@@ -7,17 +8,36 @@ class VideoScrollablleView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       physics: const BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
-      children: [
-        Container(color: Colors.red,),
-        Container(color: Colors.blue,),
-        Container(color: Colors.teal,),
-        Container(color: Colors.yellow,),
-        Container(color: Colors.deepPurple,),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+
+            //Video Player + gradiente
+
+
+
+            //Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost,))
+
+          ],
+        );
+      },
     );
   }
 }
 
+//builder, significa que va a construir los widgets bajo demanda,
+//si no lo hacemos asi, fluttr construiria todos de una vez y los
+//mantendria en memoria, lo cual es muy costoso
+
+//stack, me permite poner widgets unos sobre otros
+//positioned trabaja con el stack para posicionar los widgets
