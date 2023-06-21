4db68eeef5efeb3f2c951ae7af8f321bed249741
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-06-21 10:58:21 -0700

    Se agrego el gestureDector para pausar y reanudar el video

--- lib/widgets/video/fullscreen_player.dart
+++ lib/widgets/video/fullscreen_player.dart
@@ -42,46 +42,52 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             ),
           );
         }
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
 
-              //Gradiente
+                //Gradiente
 
-              // Texto
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption)
-                ),
-
-
-            ],
+                // Texto
+                Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(caption: widget.caption)),
+              ],
+            ),
           ),
-          );
+        );
       },
     );
   }
 }
 
-
-
 class _VideoCaption extends StatelessWidget {
-  
   final String caption;
   const _VideoCaption({super.key, required this.caption});
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     final titleStyle = Theme.of(context).textTheme.titleLarge;
 
     return SizedBox(
       width: size.width * 0.6,
-      child: Text(caption, maxLines: 2, style: titleStyle,),
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
